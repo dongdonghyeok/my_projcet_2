6703107f1c578f5214dd8395b0c972a2508193bf
dongdonghyeok <jay1415@naver.com>
2021-07-29 16:58:48 +0900

    21/07/29 secondary practice commit 1

--- profileapp/views.py
+++ profileapp/views.py
@@ -23,7 +23,7 @@ class ProfileCreateView(CreateView):
         return super().form_valid(form)
 
     def get_success_url(self):
-        return reverse('accountapp:detail', kwargs={'pk':self.object.user.pk})
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
 
 
 @method_decorator(profile_ownership_required, 'get')
@@ -35,4 +35,4 @@ class ProfileUpdateView(UpdateView):
     template_name = 'profileapp/update.html'
 
     def get_success_url(self):
-        return reverse('accountapp:detail', kwargs={'pk':self.object.user.pk})
+        return reverse('accountapp:detail', kwargs={'pk': self.object.user.pk})
