48728f8ca38444982d4ada48f2b580d3a426cd01
dongdonghyeok <jay1415@naver.com>
2021-08-19 17:09:50 +0900

    21/08/19 secondary practice commit 1

--- articleapp/models.py
+++ articleapp/models.py
@@ -2,10 +2,13 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+from projectapp.models import Project
 
 
 class Article(models.Model):
     writer = models.ForeignKey(User, on_delete=models.SET_NULL,
+                               related_name='article', null=True)
+    project = models.ForeignKey(Project, on_delete=models.SET_NULL,
                                related_name='article', null=True)
 
     title = models.CharField(max_length=200, null=True)
@@ -13,5 +16,3 @@ class Article(models.Model):
     content = models.TextField(null=True)
 
     created_at = models.DateField(auto_now_add=True, null=True)
-
-
